1b948a97dadee40bff4631478c35dd80e2ec1d72
Sai Nay LIn Htun <sunlike884@email.com>
2023-02-10 21:11:34 +0630

    express-localhost

---
+++ public/script.js
@@ -0,0 +1,13 @@
+const fetchData = async () => {
+    const API_URL = localStorage.getItem("apiUrl");
+    console.log(API_URL);
+    if (API_URL) {
+      const res = await fetch(`${API_URL}/users`);
+      const data = await res.json();
+      console.log(data);
+    } else {
+      window.location.href = "/api";
+    }
+  };
+  
+  fetchData();
