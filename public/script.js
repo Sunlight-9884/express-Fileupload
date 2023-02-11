7f22154d65e4ae334f5637679df68afb15faa907
Sai Nay LIn Htun <sunlike884@email.com>
2023-02-11 22:03:47 +0630

    change vercel

--- public/script.js
+++ public/script.js
@@ -1,15 +1,13 @@
 const fetchData = async () => {
-    const apiUrl = localStorage.getItem("apiUrl");
-    console.log();
-    if (apiUrl ) {
-      const res = await fetch(`"${apiUrl}"/users`);
-      const data = await res.json();
-      console.log(data);
-    } else {
-      window.location.href = "/api";
-    }
-  };
-  
+  const apiUrl = localStorage.getItem("apiUrl");
+  console.log();
+  if (apiUrl) {
+    const res = await fetch(`${apiUrl}/users`);
+    const data = await res.json();
+    console.log(data);
+  } else {
+    window.location.href = "/api";
+  }
+};
+
 fetchData();
-
-
